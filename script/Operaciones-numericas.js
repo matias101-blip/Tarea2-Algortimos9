5a1eb93d53e674e287c5af1cae00ca2597597aff
TheHunter101 <thehunter10121@gmail.com>
2023-11-14 13:57:27 -0500

    Goods

---
+++ script/Operaciones-numericas.js
@@ -0,0 +1,7 @@
+function suma(){
+  var numero1 = parseInt(document.getElementById("numero1").value, 10);
+  var numero2 = parseInt(document.getElementById("numero2").value, 10);
+  var sumar = numero1 + numero2;
+
+  alert(sumar);
+}
