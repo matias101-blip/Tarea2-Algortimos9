bb4c9607c6d7c858843d8c54a1d0b97dd768e1d3
TheHunter101 <thehunter10121@gmail.com>
2023-11-30 14:05:08 -0500

    codicionales

--- script/index.js
+++ script/index.js
@@ -1,11 +1,2 @@
 "use strict";
-var contenedores = document.querySelectorAll(".sub-contenedores");
-contenedores.forEach(function (contenedor) {
-    contenedor.addEventListener('click', function () {
-        debugger;
-        console.log("CLICK!!!");
-        var value = contenedor.getAttribute('data-value');
-        window.location.href = "./templates/".concat(value);
-    });
-});
 Saludar();
