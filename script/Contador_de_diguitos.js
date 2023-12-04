e5cc2ebea561f9c2d6da89b79206b09303d24e7e
TheHunter101 <thehunter10121@gmail.com>
2023-12-04 09:54:40 -0500

    Weyyy

--- script/Contador_de_diguitos.js
+++ script/Contador_de_diguitos.js
@@ -1,4 +1,36 @@
 "use strict";
+function TablaDeMultiplicar() {
+    var numero, multiplicacion, i;
+    var respuesta = document.getElementById("tabla");
+    numero = parseInt(document.getElementById("numero_mul").value);
+    for (i = 1; i < 13; i++) {
+        multiplicacion = numero * i;
+        var RespLis = document.createElement("li");
+        RespLis.textContent = "".concat(numero, " x ").concat(i, " = ").concat(multiplicacion);
+        respuesta.appendChild(RespLis);
+    }
+    return respuesta;
+}
+function Clear() {
+    var respuesta = document.getElementById("tabla");
+    while (respuesta.firstChild) {
+        respuesta.removeChild(respuesta.firstChild);
+    }
+}
+function contarVocales() {
+    debugger;
+    var frase, i = 0, vocales = 0;
+    frase = (document.getElementById("vocales").value).toLowerCase();
+    var longitud = frase.length;
+    while (i < longitud) {
+        var fraseI = frase[i];
+        if (fraseI == "a" || fraseI == "e" || fraseI == "i" || fraseI == "o" || fraseI == "u") {
+            vocales = vocales + 1;
+        }
+        i = i + 1;
+    }
+    Mensaje("Hay ".concat(vocales, " Vocales"));
+}
 function ContadorDeDigitos() {
     var text, Longitud, i, caracter;
     var contador = 0;
@@ -38,11 +70,24 @@ function Adivinar() {
     }
     numero = parseInt(document.getElementById("numero").value);
     if (numero == aleatorio) {
-        alert("Lograste adivinar!!!");
+        Mensaje("Lograste adivinar!!!");
         localStorage.clear();
     }
     else {
-        alert("Sigue intentando");
+        Mensaje("Sigue intentando");
         document.getElementById("numero").value = "";
     }
+}
+function contarAlfabeto() {
+    debugger;
+    var frase, i, vocales = 0;
+    frase = (document.getElementById("abc").value).toUpperCase();
+    var longitud = frase.length;
+    for (i = 0; i < longitud; i++) {
+        var fraseI = frase[i];
+        if (fraseI >= "A" && fraseI <= "Z") {
+            vocales = vocales + 1;
+        }
+    }
+    Mensaje("Hay ".concat(vocales, " letras del alfabeto"));
 }
--- script/prueba.js
+++ script/prueba.js
@@ -1,13 +1,1 @@
 "use strict";
-var Cadena = "H0l4";
-var i;
-for (i = 0; i < 4; i++) {
-    var numero;
-    numero = parseInt(Cadena[i]);
-    if (!isNaN(numero)) {
-        console.log("Aqui hay un numero");
-    }
-    else {
-        console.log("Aqui hay una letra");
-    }
-}
